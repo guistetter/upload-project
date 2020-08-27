89107a5426c72eea52c0711368165daf61973fda
guistetter <guilherme_stetter@hotmail.com>
2020-08-27 17:42:59 -0300

    configurando servico do s3, já esta funcionando

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+require('dotenv/config')
 const s3 = require('s3')
 const multer = require('multer')
-
 const upload = multer({dest: 'uploads'})
+
+const paramsS3 = {
+  s3Options:{
+    accessKeyId: process.env.ACCESS_ID,
+    secretAccessKey: process.env.SECRET_KEY,
+    region: 'us-east-1'
+  }
+}
+const client = s3.createClient(paramsS3)
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -14,7 +23,17 @@ app.get('/', (req, res) =>{
 })
 
 app.post('/upload', upload.single('foto'),(req, res) => {
-  res.send(req.file)
+  const params = {
+    localFile: req.file.path,
+    s3Params:{
+      Bucket: 'primeiro-bucket-s3',
+      Key: req.file.originalname
+    }
+  }
+  const uploader = client.uploadFile(params)
+  uploader.on('end', () => {
+    res.send(req.file)
+  })
 })
 
 app.listen(3000,() => console.log('running...'))
