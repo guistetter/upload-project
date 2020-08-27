cea8176c2d75a91d4a7119f678d012110bb67671
guistetter <guilherme_stetter@hotmail.com>
2020-08-27 17:32:25 -0300

    criando rota post e configurando multer, ja estamos pegando a foto e salvando temporariamente em uploads antes de enviar ao s3

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const s3 = require('s3')
+const multer = require('multer')
+
+const upload = multer({dest: 'uploads'})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) =>{
-  res.send('ola')
+  res.render('index')
+})
+
+app.post('/upload', upload.single('foto'),(req, res) => {
+  res.send(req.file)
 })
 
 app.listen(3000,() => console.log('running...'))
