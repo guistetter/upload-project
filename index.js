fbc941f654630a397e0ef134eb61f2adc326f192
guistetter <guilherme_stetter@hotmail.com>
2020-08-28 16:52:52 -0300

    Refatorando o codigo, separando as configs e funcoes

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const s3 = require('s3')
 const multer = require('multer')
 const upload = multer({dest: 'uploads'})
 const fs = require('fs')
-const paramsS3 = {
-  s3Options:{
-    accessKeyId: process.env.ACCESS_ID,
-    secretAccessKey: process.env.SECRET_KEY,
-    region: 'us-east-1'
-  }
+
+const s3Config = {
+  accessKeyId: process.env.ACCESS_ID,
+  secretAccessKey: process.env.SECRET_KEY,
+  bucket: 'primeiro-bucket-s3',
+  region: 'us-east-1'
 }
-const client = s3.createClient(paramsS3)
+// const paramsS3 = {
+//   s3Options:{
+//     accessKeyId: process.env.ACCESS_ID,
+//     secretAccessKey: process.env.SECRET_KEY,
+//     region: 'us-east-1'
+//   }
+// }
+// const client = s3.createClient(paramsS3)
+const client = s3.createClient({s3Options: s3Config})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -22,19 +30,39 @@ app.get('/', (req, res) =>{
   res.render('index')
 })
 
-app.post('/upload', upload.single('foto'),(req, res) => {
+uploadToS3 = (file, key, mimetype, s3Config) => {
   const params = {
-    localFile: req.file.path,
+    localFile: file, 
     s3Params:{
-      Bucket: 'primeiro-bucket-s3',
-      Key: req.file.originalname
+      Bucket: s3Config.bucket, 
+      Key: key,
+      ContentType: mimetype
     }
   }
-  const uploader = client.uploadFile(params)
-  uploader.on('end', () => {
-    fs.unlinkSync(req.file.path)
-    res.send(req.file)
+  return new Promise((resolve, reject) => {
+    const uploader = client.uploadFile(params)
+    uploader.on('end',()=>{
+      resolve()
+    })
   })
+}
+app.post('/upload', upload.single('foto'), async(req, res) => {
+  await uploadToS3(req.file.path, req.file.originalname, req.file.mimetype, s3Config)
+  res.send(req.file)
 })
+// app.post('/upload', upload.single('foto'),(req, res) => {
+//   const params = {
+//     localFile: req.file.path,
+//     s3Params:{
+//       Bucket: 'primeiro-bucket-s3',
+//       Key: req.file.originalname
+//     }
+//   }
+//   const uploader = client.uploadFile(params)
+//   uploader.on('end', () => {
+//     fs.unlinkSync(req.file.path)
+//     res.send(req.file)
+//   })
+// })
 
 app.listen(3000,() => console.log('running...'))
